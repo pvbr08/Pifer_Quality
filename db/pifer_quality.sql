edde97907e6cc71c1a38fa01acd948e863970cd3
PIFER\paulo.ramos <paulovitor.ramos@pifer.com.br>
2017-08-10 12:05:08 -0300

    Inclusão de campo fk_permissao em tabela de usuários

--- db/pifer_quality.sql
+++ db/pifer_quality.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `pifer_quality` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `pifer_quality`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: pifer_quality
@@ -16,8 +14,6 @@ USE `pifer_quality`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 
 --
 -- Table structure for table `avaliacao`
@@ -245,16 +241,19 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuarios` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `FK_PESSOAS` int(11) NOT NULL,
+  `FK_PESSOA` int(11) NOT NULL,
+  `FK_PERMISSAO` int(11) NOT NULL,
   `LOGIN` varchar(50) NOT NULL,
   `PASS` varchar(50) NOT NULL,
   `ATIVO` tinyint(1) NOT NULL,
-  `DT_CADASTRO` datetime NOT NULL,
   `DT_LAST_ACCESS` datetime NOT NULL,
+  `DT_CADASTRO` datetime NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `LOGIN` (`LOGIN`),
-  KEY `FK_PESSOAS` (`FK_PESSOAS`),
-  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`FK_PESSOAS`) REFERENCES `pessoas` (`ID`) ON UPDATE CASCADE
+  KEY `FK_PESSOA` (`FK_PESSOA`),
+  KEY `FK_PERMISSAO` (`FK_PERMISSAO`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`FK_PESSOA`) REFERENCES `pessoas` (`ID`) ON UPDATE CASCADE,
+  CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`FK_PERMISSAO`) REFERENCES `permissao` (`ID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -264,24 +263,9 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,1,'root','e10adc3949ba59abbe56e057f20f883e',1,'2017-08-10 10:24:38','2017-08-10 10:24:38');
+INSERT INTO `usuarios` VALUES (1,1,1,'Admin','e10adc3949ba59abbe56e057f20f883e',1,'2017-08-10 12:02:04','0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER TRG_DT_CADASTRO_USUARIOS BEFORE INSERT ON USUARIOS FOR EACH ROW SET NEW.DT_CADASTRO = NOW() */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Dumping events for database 'pifer_quality'
@@ -300,4 +284,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-10 10:33:12
+-- Dump completed on 2017-08-10 12:04:38
